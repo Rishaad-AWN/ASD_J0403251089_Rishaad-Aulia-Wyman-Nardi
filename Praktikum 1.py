6ff33f50eb4b4fc55b446670e1088c58f148e897
Rishaad-AWN <rishaadaulia4@gmail.com>
2026-02-04 20:01:13 +0700

    commit

--- Praktikum 1.py
+++ Praktikum 1.py
@@ -4,6 +4,7 @@
 #============================================================================
 
 #Contoh 1
+
 with open("Data Mahasiswa.txt", "r", encoding = "UTF-8") as file:
   data = file.read()
 print(data)
